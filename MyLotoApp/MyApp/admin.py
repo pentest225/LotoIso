2e7519143b19364fd68af298de68eead77a93eb2
pentest225 <47216215+pentest225@users.noreply.github.com>
2019-07-28 17:22:57 +0000

    Creation de la premier page
    Cherche a modifier le carousel au niveau de la taille

--- MyLotoApp/MyApp/admin.py
+++ MyLotoApp/MyApp/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.site_header = "Admin Page Super Plus "
+admin.site.site_title = "Admin "
+admin.site.index_title = "Bienvenue sur la page d'administration ;)"
+
+admin.site.register(Client)
+#@admin.register(Client)
+# class ClientAdmin (admin.ModelAdmin):
+#     # list_display = ('username' ,'email','numero','compte',)
+#     # list_filter = ("username","numero")
+#     # search_fields = ('username' ,'numero',)
+#     # list_per_page = 50
+#     # ordering = ['username',]
+
+@admin.register(Equipe)
+class EquipeAdmin (admin.ModelAdmin):
+    list_display = ('nom' ,'logo','cote',)
+    list_filter = ("nom",)
+    search_fields = ('nom' ,)
+    list_per_page = 10
+    ordering = ['nom',]
+
+
+@admin.register(Sport)
+class SportAdmin (admin.ModelAdmin):
+    list_display = ('Nom' ,'logo',)
+    list_filter = ("Nom",)
+    search_fields = ('Nom' ,)
+    list_per_page = 10
+    ordering = ['Nom',]
+
+
+@admin.register(Math)
+class MathAdmin (admin.ModelAdmin):
+    list_display = ('date_debut' ,'date_fin','id_equip1','id_equip2','scoreEq1','scoreEq2')
+    list_filter = ("id_equip1","id_equip2",)
+    search_fields = ('id_equip1','id_equip2',)
+    list_per_page = 50
+    ordering = ['id_equip1','id_equip2',]
--- MyLotoApp/MyApp/models.py
+++ MyLotoApp/MyApp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    compte_client=models.IntegerField()
+    numero=models.IntegerField()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Client.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+class Equipe(models.Model):
+    nom = models.CharField(max_length=100)
+    logo=models.CharField(max_length=500)
+    cote=models.IntegerField()
 
 # Create your models here.
+class Sport(models.Model):
+    ipEquipe=models.ForeignKey(Equipe,on_delete=models.CASCADE,null=True,related_name='Equipe_sport')
+    Nom=models.CharField(max_length=100)
+    logo=models.CharField(max_length=100)
+
+
+
+class Math(models.Model):
+    idEquipe=models.ForeignKey(Equipe,on_delete=models.CASCADE,null=True,related_name='Equipe_math' )
+    date_debut=models.DateTimeField()
+    date_fin=models.DateTimeField()
+    id_equip1=models.IntegerField(verbose_name='Equipe 1')
+    id_equip2=models.IntegerField(verbose_name='Equipe 2')
+    scoreEq1=models.IntegerField()
+    scoreEq2=models.IntegerField()
+
+
+
+class Parier(models.Model):
+    idUser=models.ForeignKey(Client,on_delete=models.CASCADE,null=True,related_name='User_parie')
+    idMath=models.ForeignKey(Math,on_delete=models.CASCADE,null=True,related_name='Math_parie')
+    mise=models.IntegerField()
+    date_parie=models.DateTimeField(auto_now_add=True)
+    date_up=models.DateTimeField(auto_now=True)
